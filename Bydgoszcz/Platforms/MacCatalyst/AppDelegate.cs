3c4c2b379e9f8e823182148eabc56a1bc9169bbb
SelinaSchuller <115994002+SelinaSchuller@users.noreply.github.com>
2024-03-15 14:50:50 +0100

    solution

---
+++ Bydgoszcz/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Bydgoszcz
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : MauiUIApplicationDelegate
+	{
+		protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+	}
+}
