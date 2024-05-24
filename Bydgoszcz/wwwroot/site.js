4c513d471f360b370fff52d579fdca43a4cf522f
SelinaSchuller <115994002+SelinaSchuller@users.noreply.github.com>
2024-05-24 13:13:15 +0200

    shit

---
+++ Bydgoszcz/wwwroot/site.js
@@ -0,0 +1,9 @@
+﻿let dotCount = 0;
+
+function startLoadingAnimation() {
+    const loadingTextElement = document.getElementById('loading-text');
+    setInterval(() => {
+        dotCount = (dotCount + 1) % 4;
+        loadingTextElement.textContent = 'Loading' + '.'.repeat(dotCount);
+    }, 1000);
+}
